acbe1b7e01ce5914b9a5ba46e1a952517b46037d
Nour Mshawrab <mshawrabnour@gmail.com>
2022-06-26 13:02:31 +0300

    Adding login api

--- src/user/router.js
+++ src/user/router.js
@@ -40,6 +40,24 @@ router.post("/register", async(req,res)=>{
   }
 })
 
+router.post("/login", async(req,res)=>{
+  try {
+    const user = await getByEmail(req.body.email);
+    if (!user) return res.status(400).send('invalid credentials');
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send('invalid credentials');
+
+    const token = jwt.sign(
+      {_id: user._id, name: user.name, email: user.email},
+      TOKEN_SECRET
+    );
+
+    return res.header('auth-token', token).send(token);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }})
 
 
 module.exports = router;
